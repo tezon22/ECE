03d9b4d0f4f193e4ef072107d26565e268a607e6
tezon <tezonteam@gmail.com>
2022-07-14 14:33:33 -0700

    HAMBURGER

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 // import Logo from './ECE LOGO With TEXT.png'
 import { Link } from 'react-router-dom'
 
+import {FaBars, FaTimes} from 'react-icons/fa'
+
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const Toggle = () => {
+    setNav(!nav)
+  }
+
+
   return (
     <div className='nav'>
       <div className="nav_container">
         <div className="logo"/>
-        <div className="right_side">
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><Link to="/section">Sections</Link></li>
-            <li><a href="google.com">Info</a></li>
-          </ul>
-          <Link to='/login' className="log">
-            LOG IN
-          </Link>
-          <Link  to='/signup' className="sign">
-            SIGN UP
-          </Link>
-         
+        <div className="">
+          <div className={ nav?  "nav-menu" : "right_side"}>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><Link to="/section">Sections</Link></li>
+              <li><a href="google.com">Info</a></li>
+            </ul>
+            <Link to='/login' className="log">
+              LOG IN
+            </Link>
+            <Link  to='/signup' className="sign">
+              SIGN UP
+            </Link>
+          </div>
+        
+          <div onClick={Toggle} className='hamburger'>
+            {nav? <FaTimes  size={20} /> : <FaBars size={20}/>}
+          </div>
        </div>
       </div>
     </div>
