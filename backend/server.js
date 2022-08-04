4e6e7625e146ff6dcb6b86bbf5f35d27bdca06fd
tezon <tezonteam@gmail.com>
2022-08-04 15:33:34 -0700

    backend added

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,17 @@ app.use(express.urlencoded({ extended: false}));
 
 app.use('/api/ece',require('./routes/registerRoutes'))
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/ece/'
+const API_URL = '/api/ece/'
 
 // Register user
 const register = async (userData) => {
