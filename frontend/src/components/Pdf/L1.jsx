485b88048142bf540d4a5b9a969565e162cac348
chidera-777 <ferdinandchidera49@gmail.com>
2023-01-01 17:34:46 +0100

    pdf and timetable

--- frontend/src/components/Pdf/L1.jsx
+++ frontend/src/components/Pdf/L1.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const L1 = () => {
   return (
-    <div>L1</div>
+      <div className="container-fluid">
+          <div className="flex my-8 mx-4">
+              <a className="w-1/12 text-5xl font-bold" href=""><i class="uil uil-previous"></i></a>
+              <div className="w-11/12 text-center text-2xl font-bold mt-2">
+                  100 Level
+              </div>       
+          </div>
+      </div>
   )
 }
 
--- frontend/src/components/Timetable/Timetable.jsx
+++ frontend/src/components/Timetable/Timetable.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const Timetable = () => {
   return (
-    <div>Timetable</div>
+      <div className="container-fluid">
+          <div className="flex my-8 mx-4">
+              <a className="w-1/12 text-5xl font-bold" href=""><i class="uil uil-previous"></i></a>
+              <div className="w-11/12 text-center text-2xl font-bold mt-2">
+                  Select your level (TimeTable)
+              </div>       
+          </div>
+          <a href="">
+              <div className="rounded-3xl text-xl bg-slate-200 text-center font-medium py-16 my-14 mx-24">
+                  100 level
+              </div>
+          </a>
+          <a href="">
+              <div className="rounded-3xl text-xl bg-slate-200 text-center font-medium py-16 my-14 mx-24">
+                  200 level
+              </div>
+          </a>
+          <a href="">
+              <div className="rounded-3xl text-xl bg-slate-200 my-14 mx-24">
+                  <p className="text-center font-medium pt-16 pb-5">300 level</p>
+                  <p className="p-4 text-right text-sm font-light">Not Available now</p>
+              </div>
+          </a>
+          <a href="">
+              <div className="rounded-3xl text-xl bg-slate-200 my-14 mx-24">
+                  <p className="text-center font-medium pt-16 pb-5">400 level</p>
+                  <p className="p-4 text-right text-sm font-light">Not Available now</p>
+              </div>
+          </a>
+          <a href="">
+              <div className="rounded-3xl text-xl bg-slate-200 my-14 mx-24">
+                  <p className="text-center font-medium pt-16 pb-5">500 level</p>
+                  <p className="p-4 text-right text-sm font-light">Not Available now</p>
+              </div>
+          </a>
+      </div>
   )
 }
 
