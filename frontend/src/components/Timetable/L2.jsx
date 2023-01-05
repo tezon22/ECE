a9aafd0806e9577df9d136284a072c3544061d4d
chidera-777 <ferdinandchidera49@gmail.com>
2023-01-05 22:03:06 +0100

    updated the desktop view of the timetable and pdf

--- frontend/src/components/Timetable/L2.jsx
+++ frontend/src/components/Timetable/L2.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const L2 = () => {
   return (
-    <div>200L</div>
+    <div className="container-fluid text-[#29335C] md:mx-[15%]">
+          <div className="flex my-8 mx-4">
+              <Link className="w-1/12 text-5xl font-bold" to="/timetable"><i class="uil uil-previous"></i></Link>
+              <div className="w-11/12 text-center text-2xl font-bold mt-2">
+                  200 Level
+              </div>       
+          </div>
+          <Link to="">
+              <div className="rounded-3xl text-xl bg-slate-200 text-center font-medium py-16 my-14 mx-20">
+                  First Semester Lectures 
+              </div>
+          </Link>
+          <Link to="">
+              <div className="rounded-3xl text-xl bg-slate-200 text-center font-medium py-16 my-14 mx-20">
+                  First Semester Exams
+              </div>
+          </Link>
+          <Link to="">
+              <div className="rounded-3xl text-xl text-center font-medium bg-slate-200 py-16 my-14 mx-20">
+                 Second Semester Lectures
+              </div>
+          </Link>
+          <Link to="">
+              <div className="rounded-3xl text-xl bg-slate-200 text-center font-medium py-16 my-14 mx-20">
+                  Second Semester Exams
+              </div>
+          </Link>
+      </div>
   )
 }
 
