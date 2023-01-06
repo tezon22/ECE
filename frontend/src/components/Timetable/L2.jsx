585ef0a4316e7eb7b2b8810a6fd6b4c0480e3254
chidera-777 <ferdinandchidera49@gmail.com>
2023-01-06 20:24:26 +0100

    fixed mobile view issue

--- frontend/src/components/Timetable/L2.jsx
+++ frontend/src/components/Timetable/L2.jsx
@@ -3,30 +3,30 @@ import { Link } from 'react-router-dom'
 
 const L2 = () => {
   return (
-    <div className="container-fluid text-[#29335C] md:mx-[15%]">
-          <div className="flex my-8 mx-4">
-              <Link className="w-1/12 text-5xl font-bold" to="/timetable"><i class="uil uil-previous"></i></Link>
-              <div className="w-11/12 text-center text-2xl font-bold mt-2">
+    <div className="container-fluid text-[#29335C] lg:mx-[15%]">
+          <div className="flex my-5 md:my-8 mx-4">
+              <Link className="w-1/12 text-3xl md:text-5xl font-bold" to="/timetable"><i class="uil uil-previous"></i></Link>
+              <div className="w-11/12 text-center text-[17px] md:text-2xl font-bold mt-2">
                   200 Level
               </div>       
           </div>
           <Link to="">
-              <div className="rounded-3xl text-xl bg-slate-200 text-center font-medium py-16 my-14 mx-20">
+              <div className="rounded-2xl md:rounded-3xl md:text-xl bg-slate-200 text-center font-medium py-12 md:py-16 my-7 md:my-14 mx-10 md:mx-20">
                   First Semester Lectures 
               </div>
           </Link>
           <Link to="">
-              <div className="rounded-3xl text-xl bg-slate-200 text-center font-medium py-16 my-14 mx-20">
+              <div className="rounded-2xl md:rounded-3xl md:text-xl bg-slate-200 text-center font-medium py-12 md:py-16 my-7 md:my-14 mx-10 md:mx-20">
                   First Semester Exams
               </div>
           </Link>
           <Link to="">
-              <div className="rounded-3xl text-xl text-center font-medium bg-slate-200 py-16 my-14 mx-20">
+              <div className="rounded-2xl md:rounded-3xl md:text-xl bg-slate-200 text-center font-medium py-12 md:py-16 my-7 md:my-14 mx-10 md:mx-20">
                  Second Semester Lectures
               </div>
           </Link>
           <Link to="">
-              <div className="rounded-3xl text-xl bg-slate-200 text-center font-medium py-16 my-14 mx-20">
+              <div className="rounded-2xl md:rounded-3xl md:text-xl bg-slate-200 text-center font-medium py-12 md:py-16 my-7 md:my-14 mx-10 md:mx-20">
                   Second Semester Exams
               </div>
           </Link>
