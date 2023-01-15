f8693c0bb1876b410e5ed396614d9c2b132736c9
tezon <tezonteam@gmail.com>
2023-01-14 23:55:27 -0800

    new

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ connectDB()
 
 const app = express()
 
-app.use(express.json());
-
 app.use(cors())
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
 app.use('/api/upload',uploadRoute)
