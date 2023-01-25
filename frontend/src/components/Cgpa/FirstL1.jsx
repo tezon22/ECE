f64b67f9272a093b2d8f6923f370d10b29f6d1d7
Andrew <anaguchidiebere@gmail.com>
2023-01-25 17:55:55 +0100

    added error handler

--- frontend/src/components/Cgpa/FirstL1.jsx
+++ frontend/src/components/Cgpa/FirstL1.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import {toast} from 'react-toastify'
 import {AiOutlineLeft} from 'react-icons/ai'
 
 const FirstL1 = () => {
@@ -15,6 +16,12 @@ const FirstL1 = () => {
       const [gst107, setGst107] = useState(null)
       const [gst109, setGst109] = useState(null)
 
+      function calculate (mat101 , feg101, phy101, ich101, ich111, bus101, phy107, gst101, gst105, gst107, gst109){
+            return (
+                  mat101 * 3 + feg101 * 2 + phy101 * 3 + ich101 * 2 + ich111 * 2 + bus101 * 2 + phy107 * 1 + gst101 * 1 + gst105 * 2 + gst107 * 2 + gst109 * 1
+            )
+      }
+      const calc = calculate (mat101, feg101, phy101, ich101, ich111, bus101, phy107, gst101, gst105, gst107, gst109)
 
   return (
     <div className="container-fluid text-[#29335C] lg:mx-[15%]">
@@ -34,7 +41,6 @@ const FirstL1 = () => {
                       <p>D -- 2</p>
                       <p>E -- 1</p>
                       <p>F -- 0</p>
-                      <p>{mat101}</p>
                 </div>
           </div>
           <div className="mb-28">
@@ -45,58 +51,58 @@ const FirstL1 = () => {
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="MAT 101" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="mat_grd" placeholder="Enter:" value={mat101} onChange={(e) => setMat101(e.target.value)}  />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="mat_grd" placeholder="Enter:" value={mat101 } onChange={(e) => setMat101(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))}  />
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="mat_unt" value={3} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="FEG 101" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="feg_grd" placeholder="Enter:" value={feg101} onChange={(e) => setFeg101(e.target.value)} />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="feg_grd" placeholder="Enter:" value={feg101} onChange={(e) => setFeg101(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))} />
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="feg_unt" value={2} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="PHY 101" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="phy_grd"  placeholder="Enter:" value={phy101} onChange={(e) => setPhy101(e.target.value)}/>
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="phy_grd"  placeholder="Enter:" value={phy101} onChange={(e) => setPhy101(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))}/>
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="phy_unt" value={3} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="ICH 101" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="ich1_grd" placeholder="Enter:" value={ich101} onChange={(e) => setIch101(e.target.value)} />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="ich1_grd" placeholder="Enter:" value={ich101} onChange={(e) => setIch101(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))} />
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="ich1_unt" value={2} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="ICH 111" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="ich2_grd"  placeholder="Enter:" value={ich111} onChange={(e) => setIch111(e.target.value)}/>
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="ich2_unt" value="2" disabled />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="ich2_grd"  placeholder="Enter:" value={ich111} onChange={(e) => setIch111(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))}/>
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="ich2_unt" value={2} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="BUS 101" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="bus_grd"  placeholder="Enter:" value={bus101} onChange={(e) => setBus101(e.target.value)}/>
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="bus_unt" value="2" disabled />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="bus_grd"  placeholder="Enter:" value={bus101} onChange={(e) => setBus101(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))}/>
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="bus_unt" value={2} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="PHY 107" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="phy2_grd" placeholder="Enter:" value={phy107} onChange={(e) => setPhy107(e.target.value)} />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="phy2_unt" value="1" disabled />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="phy2_grd" placeholder="Enter:" value={phy107} onChange={(e) => setPhy107(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))} />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="phy2_unt" value={1} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="GST 101" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="eng_grd" placeholder="Enter:" value={gst101} onChange={(e) => setGst101(e.target.value)}/>
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="eng_unt" value="1" disabled />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="eng_grd" placeholder="Enter:" value={gst101} onChange={(e) => setGst101(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))}/>
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="eng_unt" value={1} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="GST 105" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="npc_grd" placeholder="Enter:" value={gst105} onChange={(e) => setGst105(e.target.value)}/>
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="npc_unt" value="2" disabled />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="npc_grd" placeholder="Enter:" value={gst105} onChange={(e) => setGst105(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))}/>
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="npc_unt" value={2} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="GST 107" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="human_grd" placeholder="Enter:" value={gst107} onChange={(e) => setGst107(e.target.value)}/>
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="human_unt" value="2" disabled />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="human_grd" placeholder="Enter:" value={gst107} onChange={(e) => setGst107(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))}/>
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="human_unt" value={2} disabled />
                 </div>
                 <div className="flex my-5 text-[#29335C] mx-2 md:mx-4">
                       <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="text" value="GST 109" disabled />
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="igbo_grd" placeholder="Enter:" value={gst109} onChange={(e) => setGst109(e.target.value)}/>
-                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="igbo_unt" value="1" disabled />
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="igbo_grd" placeholder="Enter:" value={gst109} onChange={(e) => setGst109(e.target.value < 6 ? e.target.value : toast('Grade no dey pass 5'))}/>
+                      <input className="bg-slate-200 rounded-full py-1 text-center font-bold w-[33%] mx-2" type="number" name="igbo_unt" value={1} disabled />
                 </div>
                 <div className="md:flex my-8">
                     <p className="md:w-1/2 md:mx-10 font-bold m-4">
@@ -106,7 +112,7 @@ const FirstL1 = () => {
                           CALCULATE
                     </button>
                 </div>
-                <p className="my-4 text-2xl font-bold mx-4 text-right px-5">CGPA: </p>
+                <p className="my-4 text-2xl font-bold mx-4 text-right px-5">CGPA: {calc / 21} </p>
           </div>
         
     </div>
