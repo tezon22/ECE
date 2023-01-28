d5af41cbb49ce11a2ac2bfb9663c6bf18cd35050
Ryan-Zayne <phoenixastro97@gmail.com>
2023-01-29 00:09:57 +0100

    Added theme switch functionality

---
+++ frontend/src/components/DarkModeButton.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import useLocalStorage from 'use-local-storage';
+import { FaSun } from 'react-icons/fa';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+
+const DarkModeButton = () => {
+	const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+	function themeSetter() {
+		const newTheme = theme === 'dark' ? 'light' : 'dark';
+		setTheme(newTheme);
+	}
+
+	useEffect(() => {
+		document.documentElement.setAttribute('data-theme', theme);
+		// document.documentElement.classList.add('theme-transition');
+		// window.setTimeout(function () {
+		// 	document.documentElement.classList.remove('theme-transition');
+		// }, 1000);
+		// return () => {
+		// 	clearTimeout();
+		// };
+	}, [theme]);
+
+	return (
+		<button
+			className='bg-[rgb(32,38,64)] rounded-[5rem] absolute md:top-[0.4rem] top-[1rem] md:right-[1.5rem] right-[6rem]'
+			onClick={themeSetter}
+		>
+			<div className='relative flex gap-[0.6rem] items-center justify-between [padding-inline:0.6rem_0.5rem] [padding-block:0.3rem] w-[5.4rem] h-[2.8rem] '>
+				<FaSun color='yellow' fontSize={'1.6rem'} />
+				<BsFillMoonStarsFill color='pink' fontSize={'1.4rem'} />
+				<span
+					className={`absolute rounded-[50%] w-[2rem] aspect-square bg-white transition-transform ease-in-out duration-[750ms] ${
+						theme === 'dark' && 'translate-x-[2.4rem]'
+					}`}
+				></span>
+			</div>
+		</button>
+	);
+};
+
+export default DarkModeButton;
--- frontend/src/components/Pdf/L1.jsx
+++ frontend/src/components/Pdf/L1.jsx
@@ -1,50 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {AiFillFolder} from 'react-icons/ai'
-import { AiOutlineLeft } from 'react-icons/ai'
-import Navbar from '../Navbar/Navbar'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiFillFolder } from 'react-icons/ai';
+import { AiOutlineLeft } from 'react-icons/ai';
+import Navbar from '../Navbar/Navbar';
 
 const L1 = () => {
-   
-    const courses1 = ['MAT 101', 'FEG 101', 'PHY 101','ICH 101', 'ICH 111']
-    const courses2 = ['MAT 102', 'FEG 102', 'PHY 102', 'ICH 102', 'ICH 112', 'FEG 103']
-    return (
-      <div>
-        <Navbar/>
-            
-        <div className="text-[#29335C] lg:mx-[15%]">
-            <div className="flex my-5 md:my-8 mx-4">
-                <Link className="w-1/12 text-2xl md:text-4xl font-bold" to="/ebook" ><AiOutlineLeft/></Link>
-                <div className="w-11/12 text-center text-[17px] md:text-2xl font-bold mt-2">
-                    100 Level
-                </div>       
-            </div>
-            <div className="mx-8 md:mx-14 mb-24 md:mb-28">
-                <p className="font-semibold md:text-2xl mt-10 md:mt-14 mb-6 md:mb-8">First Semester</p>
-               {courses1.map((course1) =>{
-                return (
-                    <Link  to={{pathname: `/pdf/${course1}`}} state={{from: course1}} key={course1}>
-                    <div className="my-6 md:my-8 p-3 md:p-4 bg-slate-200 rounded-full font-semibold text-sm md:text-xl">
-                        {course1} <span className="text-xl md:text-2xl float-right"><AiFillFolder/></span>
-                    </div>
-                </Link>
-                )
-               })}
-                <p className="font-semibold md:text-2xl mt-10 md:mt-14 mb-6 md:mb-8">Second Semester</p>
-                {courses2.map((course2) =>{
-                return (
-                    <Link to={`/pdf/${course2}`} state={{from: course2}}  key={course2}>
-                    <div className="my-6 md:my-8 p-3 md:p-4 bg-slate-200 rounded-full font-semibold text-sm md:text-xl">
-                        {course2} <span className="text-xl md:text-2xl float-right"><AiFillFolder/></span>
-                    </div>
-                </Link>
-                )
-               })}
-            </div>
-        </div>
-      </div>
-  )
-}
+	const courses1 = ['MAT 101', 'FEG 101', 'PHY 101', 'ICH 101', 'ICH 111'];
+	const courses2 = ['MAT 102', 'FEG 102', 'PHY 102', 'ICH 102', 'ICH 112', 'FEG 103'];
+	return (
+		<div>
+			<Navbar />
 
-export default L1
+			<div className='text-[#29335C] lg:mx-[15%]'>
+				<div className='flex my-5 md:my-8 mx-4'>
+					<Link className='w-1/12 text-2xl md:text-4xl font-bold' to='/ebook'>
+						<AiOutlineLeft />
+					</Link>
+					<div className='w-11/12 text-center text-[17px] md:text-2xl font-bold mt-2'>100 Level</div>
+				</div>
+				<div className='mx-8 md:mx-14 mb-24 md:mb-28'>
+					<p className='font-semibold md:text-2xl mt-10 md:mt-14 mb-6 md:mb-8'>First Semester</p>
+					{courses1.map((course1) => {
+						return (
+							<Link to={{ pathname: `/pdf/${course1}` }} state={{ from: course1 }} key={course1}>
+								<div className='my-6 md:my-8 p-3 md:p-4 bg-[var(--light-black,_rgb(226,232,240))] rounded-full font-semibold text-sm md:text-xl'>
+									{course1}{' '}
+									<span className='text-xl md:text-2xl float-right'>
+										<AiFillFolder />
+									</span>
+								</div>
+							</Link>
+						);
+					})}
+					<p className='font-semibold md:text-2xl mt-10 md:mt-14 mb-6 md:mb-8'>Second Semester</p>
+					{courses2.map((course2) => {
+						return (
+							<Link to={`/pdf/${course2}`} state={{ from: course2 }} key={course2}>
+								<div className='my-6 md:my-8 p-3 md:p-4 bg-[var(--light-black,_rgb(226,232,240))] rounded-full font-semibold text-sm md:text-xl'>
+									{course2}{' '}
+									<span className='text-xl md:text-2xl float-right'>
+										<AiFillFolder />
+									</span>
+								</div>
+							</Link>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default L1;
