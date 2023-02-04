98a33a05c7d3fcd7a6e2cadfdb62810b680b956e
tezon <tezonteam@gmail.com>
2023-02-03 22:01:57 -0800

    reset password

--- backend/routes/resetPassword.js
+++ backend/routes/resetPassword.js
@@ -2,41 +2,35 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
-const User = require("../models/userModel");
-const Token = require("../models/token");
+const {User} = require("../models/userModel");
+const Token = require("../models/tokenModel");
 const AppError = require("../utils/appError");
 const router = express.Router();
 
 
 router.patch("/resetpassword/:token", async (req, res, next) => {
-  // get user based on the token
-  // const id = req.params.id, 
-  const hashedToken = crypto
-    .createHash("sha256")
-    .update(req.params.token)
-    .digest("hex");
+  try {
+    const schema = Joi.object({ password: Joi.string().required() });
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  const user = await User.findOne({
-    passwordResetToken: hashedToken,
-    passwordResetExpires: { $gt: Date.now() },
-  });
-  // if token has not expired, and there is user, set the new password
-  if (!user) {
-    return next(new AppError("Token is invalid or has expired", 400));
-  }
-  user.password = req.body.password;
-  user.passwordConfirm = req.body.passwordConfirm;
-  user.passwordResetToken = undefined;
-  user.passwordResetExpires = undefined;
-  await user.save();
+    const user = await User.findById(req.params.userId);
+    if (!user) return res.status(400).send("invalid link or expired");
 
-  // update changePasswordAt property for the user
-  // log the user in, send JWT
-    const token = signToken(user._id);
-
-    res.status(200).json({
-      status: 'success',
-      token
+    const token = await Token.findOne({
+        userId: user._id,
+        token: req.params.token,
     });
+    if (!token) return res.status(400).send("Invalid link or expired");
+
+    user.password = req.body.password;
+    await user.save();
+    await token.delete();
+
+    res.send("password reset sucessfully.");
+} catch (error) {
+    res.send("An error occured");
+    console.log(error);
+}
 });
 module.exports = router;
