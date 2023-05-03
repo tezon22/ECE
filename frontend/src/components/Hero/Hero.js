7c62f6391de81286beff8480f5ecf85e77947b9f
Ikedigwe Ebube <93886198+Ryan-Zayne@users.noreply.github.com>
2023-05-03 20:42:40 +0100

    Swapped out images (#135)

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import './Hero.css'
-import Image1 from './images/image 1.png'
-import Vector from './images/Vector.png'
-import Vector12 from './images/Vector (12).png'
-import { Link } from 'react-router-dom'
-
+import React from 'react';
+import './Hero.css';
+import Image1 from './images/image 1.svg';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
-    return (
-        <div>
-            <section className="hero_container">
-                <div className="hero_row">
-                    <div className="col_1">
-                        <h2>FACULTY OF ENGINEERING</h2>
-                        <h1>Department of <span>Electronics</span> and <span>Computer</span> Engineering</h1>
-                        <p>A plug into an engineering digital library, <br class="hero_hide" />Let's study.</p>
-                        <Link to="/signup">Get Started</Link>
-                        <img src={Vector} alt='vector' className='vector' />
-                        <img src={Vector12} alt='vector12' className='vector12' />
-                    </div>
-                    <div className="col_2">
-                        <img src={Image1} alt='image1' />
-                    </div>
-                </div>
-            </section>
-        </div>
-    )
-}
+	return (
+		<>
+			<section className="hero_container">
+				<div className="hero_row">
+					<div className="col_1">
+						<h2>FACULTY OF ENGINEERING</h2>
+						<h1>
+							Department of <span>Electronics</span> and <span>Computer</span> Engineering
+						</h1>
+						<p>
+							A plug into an engineering digital library, <br className="hero_hide" />
+							Let's study.
+						</p>
+						<Link to="/signup">Get Started</Link>
+					</div>
+					<div className="col_2">
+						<img src={Image1} alt="image1" />
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
 
-export default Hero
+export default Hero;
