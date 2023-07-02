34bb7cf0e507eda5ace6fa5030d499a8621e697a
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2023-07-02 13:18:31 +0100

    added pdf upload using filestack api

--- frontend/src/components/Pdf/Upload.jsx
+++ frontend/src/components/Pdf/Upload.jsx
@@ -1,59 +1,56 @@
-import { useState } from "react";
 import { cloudinaryUpload } from "../../hooks/cloudinaryUpload";
+import * as filestack from 'filestack-js'
+import { useState } from "react";
 
 const Upload = () => {
-  const [PDF, setPDF] = useState("");
-  const fields = [
-    "Filename:",
-    "Size:",
-    "URL:",
-    "Author:",
-    "Keyword:",
-    "Title:",
-  ];
-  const form = new FormData();
-  form.append("file", PDF);
-  form.append("upload_preset", "wxilnhxo");
-  form.append("cloud_name", "dscbiu2km");
-
+  const [fileName, setFileName] = useState("")
+  const [response, setResponse] = useState("")
+  const client = filestack.init('AqUp2oJ2SvGVXXPWfLVgaz');
   const submit = () => {
-    cloudinaryUpload(form);
+    const options = {
+      accept: ['application/pdf', 'image/*'], // Specify the accepted file types
+      maxFiles: 5, // Set the maximum number of files to be uploaded
+      storeTo: {
+        location: 's3', // Specify the storage location (e.g., S3)
+      },
+      onFileUploadFinished(file){
+        console.log(file);
+      },
+      uploadConfig: {
+        tags: {
+          fileName,
+        },
+      },   
+    };
+    
+    client.picker(options).open().then((response) => setResponse(response));
   };
-  console.log(PDF);
+  console.log(response)
   return (
     <div className="flex flex-col ">
       <div className="flex flex-col text-center gap-20">
         <h1 className="text-[#29335c] text-[35px] font-bold">
           Uploading of PDF's
         </h1>
-        <a
-          href="https://res.cloudinary.com/dscbiu2km/image/upload/v1687047722/PDF/iiogljt1baj4fyirytzh.pdf"
+        <p
           className="text-[12px]"
         >
           Fill this form to sucessfully upload a PDF
-        </a>
+        </p>
       </div>
-      {fields.map((field) => {
-        return (
-          <div className="flex w-[100%]" key={field}>
-            <input
-              className="mx-auto my-3 p-8 w-[80%] bg-[var(--light-black,_rgb(226,232,240))] rounded-[10px] font-semibold text-xl md:text-2xl"
-              placeholder={field}
-            />
-          </div>
-        );
-      })}
+      <div>
       <input
-        onChange={(e) => setPDF(e.target.files[0])}
-        type="file"
-        className="mx-auto my-3 p-8 w-[80%] bg-[var(--light-black,_rgb(226,232,240))] rounded-[10px] font-semibold text-xl md:text-2xl"
-      />
+              className="mx-auto my-3 p-8 w-[80%] bg-[var(--light-black,_rgb(226,232,240))] rounded-[10px] font-semibold text-xl md:text-2xl"
+              placeholder="FileName"
+              onChange={(e)=> setFileName(e.target.value)}
+            />
       <button
         onClick={submit}
         className="bg-red p-5 text-white font-bold w-[30%] mx-auto rounded-[10px] mt-4"
       >
-        Submit
+        Upload File
       </button>
+      </div>
     </div>
   );
 };
--- frontend/src/hooks/cloudinaryUpload.js
+++ frontend/src/hooks/cloudinaryUpload.js
@@ -1,16 +1,10 @@
 import axios from 'axios'
  
-const url = "https://api.cloudinary.com/v1_1/dscbiu2km/image/upload"
-export const cloudinaryUpload = (body) =>{
-    
-    const response = axios.post(url, body)
-    .then((res)=>console.log(res.data))
-    .catch((err)=>console.log(err.response.data.error.message))
-
-    // for reaponse we have res.data.pages and original_filename and bytes and url
-    // console.log(response);
-    // const url = "https://res.cloudinary.com/dscbiu2km/image/upload/v1687048319/PDF/h56gv5c3mad2azhnm1ac.pdf"
-// const Image = url.replace(".pdf", ".jpg")
-// console.log(Image)   
+const url = "/api/upload"
+export const cloudinaryUpload = (data) =>{
+    console.log(data)
+    // const response = axios.post(url, data)
+    // .then((res)=>console.log(res.data))
+    // .catch((err)=>console.log(err.response.data.error.message))
+    // console.log(response, data)  
 }
-//secure_url
